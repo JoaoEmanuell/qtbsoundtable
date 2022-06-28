a8b8f41a9b3b10d7159b3aec0a133a18fed91b5a
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-06-28 18:26:25 -0300

    app.py/load_ui

---
+++ qtbsoundtable/app.py
@@ -0,0 +1,31 @@
+from typing import Type
+from pathlib import Path
+from os.path import join
+
+from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtUiTools import QUiLoader
+from PySide6.QtCore import QFile
+
+class App():
+    def __init__(self) -> None:
+        self.__app = QApplication([])
+
+        # Add form
+
+        self.__add_form = self.load_ui('add_songs.ui')
+
+        self.__add_form.show()
+
+        self.__app.exec()
+
+    def load_ui(self, ui_file : str) -> Type[QWidget]:
+        path = Path().absolute()
+        path = join(path, 'qtbsoundtable', "layouts", "")
+        ui_file = QFile(f'{path}{ui_file}')
+        ui_file.open(QFile.ReadOnly)
+        loader = QUiLoader()
+        ui = loader.load(ui_file)
+        return ui
+
+if __name__ == '__main__':
+    App()
