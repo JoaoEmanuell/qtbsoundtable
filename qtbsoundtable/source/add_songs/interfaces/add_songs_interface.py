89042d26ef1d6a85137195ad29d6ca734c96d711
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-06-28 21:03:43 -0300

    docstrings addsongsinterface

--- qtbsoundtable/source/add_songs/interfaces/add_songs_interface.py
+++ qtbsoundtable/source/add_songs/interfaces/add_songs_interface.py
@@ -4,8 +4,17 @@ from typing import List
 class AddSongsInterface(ABC):
     @abstractmethod
     def __init__(self, paths : List[str], absolute_path : str) -> None:
+        """Init
+
+        Args:
+            paths (List[str]): Paths to original songs
+            absolute_path (str): Absolute path to application
+        """        
         raise NotImplementedError()
 
     @abstractmethod
     def add_songs(self) -> None:
+        """Copy songs to songs directory
+
+        """        
         raise NotImplementedError()
