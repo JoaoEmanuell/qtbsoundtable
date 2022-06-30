5a1fcd68c7e901c29dcf3f57fbdbcfdc47bb943a
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-06-30 17:39:10 -0300

    Card song

---
+++ qtbsoundtable/source/card_song/card_song.py
@@ -0,0 +1,51 @@
+from typing import List, Type
+from PySide6.QtWidgets import QWidget, QGridLayout, QLabel, QPushButton, QComboBox
+from PySide6.QtCore import QRect
+
+from .interfaces import CardSongInterface
+
+class CardSong(CardSongInterface):
+    def __init__(self, id: int, song_name: str, short_cuts: List[str]) -> None:
+        self.__id = str(id)
+        self.__song_name = song_name
+        self.__short_cuts = (*short_cuts, )
+
+    def create_card_song(self) -> Type[QGridLayout]:
+        gridLayoutWidget = QWidget()
+        gridLayoutWidget.setObjectName(r"gridLayoutWidget")
+        gridLayoutWidget.setGeometry(QRect(130, 50, 299, 219))
+
+        gridLayout = QGridLayout(gridLayoutWidget)
+        gridLayout.setObjectName(r"gridLayout")
+        gridLayout.setContentsMargins(0, 0, 0, 0)
+
+        # Combo Box | Short cuts
+
+        comboBox = QComboBox(gridLayoutWidget)
+        comboBox.setObjectName(r"shortCuts")
+        comboBox.addItems([*self.__short_cuts])
+
+        # Song name
+
+        song_name = QLabel(gridLayoutWidget)
+        song_name.setText(self.__song_name)
+
+        # Shortcut label
+
+        short_cut_label = QLabel(gridLayoutWidget)
+        short_cut_label.setText(r"Atalho : ")
+
+        # Play button
+
+        play_button = QPushButton(gridLayoutWidget)
+        play_button.setText(r"Play")
+        play_button.setObjectName(self.__id)
+
+        # Add in grid
+        
+        gridLayout.addWidget(comboBox, 2, 1, 1, 1)
+        gridLayout.addWidget(song_name, 1, 0, 1, 2)
+        gridLayout.addWidget(short_cut_label, 1, 0, 1, 2)
+        gridLayout.addWidget(play_button, 3, 0, 1, 2)
+
+        return gridLayout
