8d59fc2de6f3b96d248634cbc4680d63bd3715b4
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-07-07 18:39:15 -0300

    Multi thread test > Test Multi Thread class

--- tests/multi_thread_test.py
+++ tests/multi_thread_test.py
@@ -1,7 +1,10 @@
 from sys import path
+from time import sleep
 path.append('../')
 
+from qtbsoundtable.source import Factory
 from qtbsoundtable.source.multi_thread.multi_thread_error import MultiThreadError
+from qtbsoundtable.source.multi_thread import MultiThreadInterface
 
 def test_answer():
     # Error class
@@ -10,3 +13,51 @@ def test_answer():
         raise multi_thread_error
     except MultiThreadError:
         assert True
+
+    # Multi Thread
+
+    fac = Factory()
+
+    multi_thread : MultiThreadInterface \
+        = fac.get_representative(MultiThreadInterface)
+
+    assert isinstance(multi_thread, MultiThreadInterface)
+
+    thread_id = multi_thread.create_thread(target=__test_thread, args=(5,))
+
+    assert type(thread_id) == int
+
+    sleep(0.5)
+
+    # Restart
+
+    multi_thread.start_thread(thread_id)
+
+    # Stop
+
+    multi_thread.stop_thread(thread_id)
+
+    # Is alive
+
+    assert multi_thread.is_alive(thread_id) == True
+
+    # Delete
+
+    multi_thread.delete_thread(thread_id)
+
+    # Limit
+
+    try:
+        for _ in range(0, 15):
+            id = multi_thread.create_thread(target=__test_thread, args=(10,))
+            multi_thread.stop_thread(id)
+    except MultiThreadError:
+        pass
+
+def __test_thread(max: int):
+    for i in range(max):
+        sleep(0.1)
+        print(i)
+
+if __name__ == '__main__':
+    test_answer()
