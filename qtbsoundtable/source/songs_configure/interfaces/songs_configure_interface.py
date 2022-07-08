df58a42ded5d81e555e0ba37a2e1244a14c953e2
João Emanuel da Silva Mendonça <ggg2j15@gmail.com>
2022-07-07 21:29:36 -0300

    Update songs configure and songs configure interface

--- qtbsoundtable/source/songs_configure/interfaces/songs_configure_interface.py
+++ qtbsoundtable/source/songs_configure/interfaces/songs_configure_interface.py
@@ -2,25 +2,29 @@ from abc import ABC, abstractmethod
 from typing import Type
 
 from ...songs_json_manipulation.interfaces import SongsJsonManipulationInterface
+from ...multi_thread import MultiThreadInterface
 
 class SongsConfigureInterface(ABC):
-    """Songs configure, get all songs from json, play song."""    
+    """Songs configure, play song."""    
     @abstractmethod
     def __init__(
         self, 
         absolute_path: str, 
-        songs_json_manipulation : Type[SongsJsonManipulationInterface]
+        songs_json_manipulation : Type[SongsJsonManipulationInterface],
+        thread_manipulation : Type[MultiThreadInterface]
         ) -> None:
         """Init
 
         Args:
             absolute_path (str): Absolute path to app.py, to locate a songs.json
+            songs_json_manipulation(Type[SongsJsonManipulationInterface]): Songs json manipulation
+            thread_manipulation(Type[MultiThreadInterface]): Thread manipulation
 
         """        
         raise NotImplementedError()
 
     @abstractmethod
-    def play_song(self, id: int, loop: bool) -> None:
+    def play_song(self, id: int=0, loop: bool=False) -> int:
         """Play a song
 
         Args:
@@ -29,11 +33,19 @@ class SongsConfigureInterface(ABC):
 
         Raises:
             FileNotFoundError: Case the song does not exist
-        """        
+
+        Returns:
+            int: Thread Id, provide by MultiThread
+        """            
         raise NotImplementedError()
         raise FileNotFoundError()
 
     @abstractmethod
-    def stop_song(self) -> None:
-        """Stop a song"""        
+    def stop_song(self, thread_id: int=0) -> None:
+        """Stop a song
+
+        Args:
+            thread_id (int): Thread id provide by play_song
+
+        """        
         raise NotImplementedError()
--- qtbsoundtable/source/songs_configure/songs_configure.py
+++ qtbsoundtable/source/songs_configure/songs_configure.py
@@ -3,10 +3,10 @@ from typing import Type
 
 from pydub import AudioSegment
 from pydub.playback import _play_with_simpleaudio
-from simpleaudio import PlayObject
 
 from .interfaces import SongsConfigureInterface
 from ..songs_json_manipulation.interfaces import SongsJsonManipulationInterface
+from ..multi_thread import MultiThreadInterface
 
 class SongsConfigure(SongsConfigureInterface):
     def __init__(
@@ -17,9 +17,9 @@ class SongsConfigure(SongsConfigureInterface):
 
         self.__absolute_path = absolute_path
         self.__songs_json_manipulation = songs_json_manipulation
-        self.__song_thread : PlayObject = PlayObject
+        self.__multi_thread : MultiThreadInterface = MultiThreadInterface
 
-    def play_song(self, id: int, loop: bool) -> None:
+    def play_song(self, id: int=0, loop: bool=False) -> int:
         if not exists(join(self.__absolute_path, 'songs.json')):
             raise FileNotFoundError()
 
@@ -27,12 +27,13 @@ class SongsConfigure(SongsConfigureInterface):
 
         song_to_play = AudioSegment.from_file(song)
 
-        self.__song_thread = _play_with_simpleaudio(song_to_play)
+        thread_id = self.__multi_thread.create_thread(
+            target=_play_with_simpleaudio, 
+            args=(song_to_play,)
+        )
         print(f"Playing song: {song}")
+        return thread_id
 
-    def stop_song(self) -> None:
-        try :
-            self.__song_thread.stop()
-        except RuntimeError:
-            pass
-        self.__song_thread = PlayObject
+    def stop_song(self, thread_id: int=0) -> None:
+        self.__multi_thread.stop_thread(thread_id)
+        self.__multi_thread.delete_thread(thread_id)
